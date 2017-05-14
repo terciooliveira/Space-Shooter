865a9e2261943e76db88db985c578f0b1b9f8ca9
Tércio Oliveira de Almeida <tercio.coritiba@gmail.com>
2017-05-14 01:58:43 -0300

    Adicionando um background temporario para a cena de teste e realizando alguns testes com layers

---
+++ Assets/TestScriptDeleteMePlease.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestScriptDeleteMePlease : MonoBehaviour {
+
+    public GameObject explosion;
+
+	// Use this for initialization
+	void Start () {
+        StartCoroutine(createMultipleExplisions());
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    IEnumerator createMultipleExplisions()
+    {
+        yield return new WaitForSeconds(3.0f);
+
+        while (true)
+        {
+            Vector3 position = new Vector3(transform.position.x, transform.position.y, transform.position.z + 3);
+            Instantiate(explosion, position, transform.rotation);
+
+            yield return new WaitForSeconds(3.0f);
+        }
+    }
+}
